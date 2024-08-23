16ed20bf1455a05a0d813228f001717ff75f822b
mnenie <121057011+mneniee@users.noreply.github.com>
2024-08-23 19:54:01 +0300

    fix: 🔧 media queries and styles

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -19,14 +19,14 @@ const { scrollToBlock } = useNavigation()
     class="fixed w-full bg-[#111827]/75 backdrop-blur -mb-px top-0 z-50 lg:mb-0 border-0"
   >
     <div
-      class="container flex h-14 max-w-screen-2xl items-center justify-between"
+      class="md:container pl-4 pr-4 flex h-14 max-w-screen-2xl items-center justify-between"
     >
       <div class="flex items-center">
         <span class="text-[20px] font-bold text-zinc-100">{{ t('header.name') }}</span>
         <div class="h-4 w-px bg-zinc-600 ml-4 mr-4" />
         <I18nLanguageSwitcher />
       </div>
-      <div class="flex items-center space-x-10">
+      <div class="flex items-center space-x-4 md:space-x-6 lg:space-x-10">
         <span
           v-for="link in localeLinks"
           :key="link.id"
