8b1f8f330f32bd01f9402eed2f06bbe9d59ab10b
mnenie <1alexpeshkov@gmail.com>
2024-08-30 23:56:53 +0300

    fix: 🔧 some styles

--- components/projects/OnePart.vue
+++ components/projects/OnePart.vue
@@ -8,39 +8,39 @@ const { goToBlankPage } = useNavigation()
 
 <template>
   <div
-    class="relative rounded-lg p-5 shadow-[0_20px_40px_20px_rgb(0,220,130,0.2)]"
+    class="flex flex-col justify-between relative rounded-lg p-5 pb-4 shadow-[0_20px_40px_20px_rgb(0,220,130,0.2)]"
   >
-    <div class="relative 2xl:mb-4 mb-6">
-      <div
-        class="w-full rounded-lg relative lg:h-[210px] block_proj overflow-hidden"
-      >
-        <img
-          class="rounded-lg lg:h-[210px] w-full block_proj"
-          :src="project.img"
-        >
+    <div class="flex flex-col">
+      <div class="relative 2xl:mb-4 mb-6">
         <div
-          class="absolute right-4 top-4 rounded-full w-10 h-10 bg-gray-900 flex items-center justify-center cursor-pointer"
-          @click="goToBlankPage(project.github)"
+          class="w-full rounded-lg relative lg:h-[210px] block_proj overflow-hidden"
         >
-          <img class="w-10 h-10 object-contain" src="/projects/github.png">
-        </div>
-        <div
-          v-if="project && project.product"
-          class="absolute right-16 top-4 rounded-full w-10 h-10 bg-gray-900 flex items-center justify-center cursor-pointer"
-          @click="goToBlankPage(project.product)"
-        >
-          <img class="w-6 h-6 object-contain" src="/projects/site.svg">
+          <img
+            class="rounded-lg lg:h-[210px] w-full block_proj"
+            :src="project.img"
+          >
+          <div
+            class="absolute right-4 top-4 rounded-full w-10 h-10 bg-gray-900 flex items-center justify-center cursor-pointer"
+            @click="goToBlankPage(project.github)"
+          >
+            <img class="w-10 h-10 object-contain" src="/projects/github.png">
+          </div>
+          <div
+            v-if="project && project.product"
+            class="absolute right-16 top-4 rounded-full w-10 h-10 bg-gray-900 flex items-center justify-center cursor-pointer"
+            @click="goToBlankPage(project.product)"
+          >
+            <img class="w-6 h-6 object-contain" src="/projects/site.svg">
+          </div>
         </div>
       </div>
-    </div>
-    <div class="flex flex-col justify-between h-fit relative">
       <div class="flex justify-between items-center">
-        <p class="text-white text-xl mb-[62px]">
+        <p class="text-white text-xl mb-[20px]">
           {{ project.title }}
         </p>
       </div>
     </div>
-    <span class="mb-2 text-white xl:text-base text-lg absolute bottom-2 pr-5">{{ project.stack }}</span>
+    <span class="text-white 2xl:text-base text-lg">{{ project.stack }}</span>
   </div>
 </template>
 
